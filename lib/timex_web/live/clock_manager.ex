dae1cafe37a4c33cc9702cc79cee9224a3cdaac8
Luciano Garcia-Banuelos <luciano.garcia@tec.mx>
2023-05-23 12:03:25 -0600

    Time + Stopwatch partially complete

--- lib/timex_web/live/clock_manager.ex
+++ lib/timex_web/live/clock_manager.ex
@@ -6,13 +6,15 @@ defmodule TimexWeb.ClockManager do
     {_, now} = :calendar.local_time()
 
     Process.send_after(self(), :working_working, 1000)
-    {:ok, %{ui_pid: ui, time: Time.from_erl!(now), st: Working}}
+    {:ok, %{ui_pid: ui, time: Time.from_erl!(now), st: Working, mode: Time}}
   end
 
-  def handle_info(:working_working, %{ui_pid: ui, time: time, st: Working} = state) do
+  def handle_info(:working_working, %{ui_pid: ui, time: time, st: Working, mode: mode} = state) do
     Process.send_after(self(), :working_working, 1000)
     time = Time.add(time, 1)
-    GenServer.cast(ui, {:set_time_display, Time.truncate(time, :second) |> Time.to_string })
+    if mode == Time do
+      GenServer.cast(ui, {:set_time_display, Time.truncate(time, :second) |> Time.to_string })
+    end
     {:noreply, state |> Map.put(:time, time) }
   end
 
--- lib/timex_web/live/stopwatch_manager.ex
+++ lib/timex_web/live/stopwatch_manager.ex
@@ -3,6 +3,47 @@ defmodule TimexWeb.StopwatchManager do
 
   def init(ui) do
     :gproc.reg({:p, :l, :ui_event})
-    {:ok, %{ui_pid: ui, count: ~T[00:00:00.00]}}
+    GenServer.cast(ui, {:set_time_display, ~T[00:00:00.00] |> Time.truncate(:millisecond) |> Time.to_string |> String.slice(3..-1)})
+    {:ok, %{ui_pid: ui, count: ~T[00:00:00.00], st1: Working, st2: Paused, mode: Time}}
+  end
+
+  def handle_info(:"top-left", %{st1: Working, mode: mode, ui_pid: ui, count: count} = state) do
+    IO.puts("IN .... abc")
+    mode =
+      if mode == Time do
+        GenServer.cast(ui, {:set_time_display, count |> Time.truncate(:millisecond) |> Time.to_string |> String.slice(3..-2)})
+        SWatch
+      else
+        Time
+      end
+    {:noreply, %{state | mode: mode}}
+  end
+
+  def handle_info(:"bottom-left", %{ui_pid: ui, st1: Working} = state) do
+    GenServer.cast(ui, {:set_time_display, ~T[00:00:00.00] |> Time.truncate(:millisecond) |> Time.to_string |> String.slice(3..-1)})
+    {:noreply, %{state | count: ~T[00:00:00.00]}}
+  end
+
+  def handle_info(:"bottom-right", %{st2: Paused, mode: SWatch} = state) do
+    Process.send_after(self(), Counting_to_Counting, 10)
+    {:noreply, %{state | st2: Counting}}
+  end
+
+  def handle_info(:"bottom-right", %{st2: Counting, mode: SWatch} = state) do
+    {:noreply, %{state | st2: Paused}}
+  end
+
+  def handle_info(Counting_to_Counting, %{st2: Counting, count: count, ui_pid: ui, mode: mode} = state) do
+    Process.send_after(self(), Counting_to_Counting, 10)
+    count = Time.add(count, 10, :millisecond)
+    if mode == SWatch do
+      GenServer.cast(ui, {:set_time_display, count |> Time.truncate(:millisecond) |> Time.to_string |> String.slice(3..-2)})
+    end
+    {:noreply, %{state | st2: Counting, count: count}}
+  end
+
+  def handle_info(event, state) do
+    IO.inspect(event)
+   {:noreply, state}
   end
 end
