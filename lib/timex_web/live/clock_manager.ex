455e4964590b38ee2cc9fefd2ba978d542a5cd40
Luciano Garcia-Banuelos <luciano.garcia@tec.mx>
2023-05-26 11:54:45 -0600

    Indiglo already working

--- lib/timex_web/live/clock_manager.ex
+++ lib/timex_web/live/clock_manager.ex
@@ -5,15 +5,20 @@ defmodule TimexWeb.ClockManager do
     :gproc.reg({:p, :l, :ui_event})
     {_, now} = :calendar.local_time()
 
+    alarm = Time.add(Time.from_erl!(now), 60)
+
     Process.send_after(self(), :working_working, 1000)
-    {:ok, %{ui_pid: ui, time: Time.from_erl!(now), st: Working, mode: Time}}
+    {:ok, %{ui_pid: ui, time: Time.from_erl!(now), st: Working, mode: Time, alarm: alarm}}
   end
 
-  def handle_info(:working_working, %{ui_pid: ui, time: time, st: Working, mode: mode} = state) do
+  def handle_info(:working_working, %{ui_pid: ui, time: time, st: Working, mode: mode, alarm: alarm} = state) do
     Process.send_after(self(), :working_working, 1000)
     time = Time.add(time, 1)
     if mode == Time do
       GenServer.cast(ui, {:set_time_display, Time.truncate(time, :second) |> Time.to_string })
+    end
+    if time == alarm do
+      :gproc.send({:p, :l, :ui_event}, :start_alarm)
     end
     {:noreply, state |> Map.put(:time, time) }
   end
--- lib/timex_web/live/indiglo_manager.ex
+++ lib/timex_web/live/indiglo_manager.ex
@@ -3,6 +3,26 @@ defmodule TimexWeb.IndigloManager do
 
   def init(ui) do
     :gproc.reg({:p, :l, :ui_event})
-    {:ok, %{ui_pid: ui}}
+    {:ok, %{ui_pid: ui, st: IndigloOff}}
   end
+
+  # IndigloOff -- top-right/set_indiglo() --> IndigloOn
+  def handle_info(:"top-right", %{st: IndigloOff, ui_pid: ui} = state) do
+    GenServer.cast(ui, :set_indiglo)
+    {:noreply, state |> Map.put(:st, IndigloOn) }
+  end
+
+  # IndigloOn -- top-right(released) --> Waiting
+  def handle_info(:"top-right", %{st: IndigloOn} = state) do
+    Process.send_after(self(), Waiting_IndigloOff, 2000)
+    {:noreply, state |> Map.put(:st, Waiting)}
+  end
+
+  # Waiting -- after 2s --> IndigloOff
+  def handle_info(Waiting_IndigloOff, %{st: Waiting, ui_pid: ui} = state) do
+    GenServer.cast(ui, :unset_indiglo)
+    {:noreply, state |> Map.put(:st, IndigloOff)}
+  end
+
+  def handle_info(_event, state), do: {:noreply, state}
 end
